06b86fe9364720c2e9357018ed591ba8c38864d2
nqnoname <115331491+nqnoname@users.noreply.github.com>
2022-11-12 12:38:19 +0700

    Update letEncryptWildcardSSL.sh

--- letEncryptWildcardSSL.sh
+++ letEncryptWildcardSSL.sh
@@ -1,14 +1,16 @@
 # ensure that you have the latest version of snapd
-sudo snap install core; sudo snap refresh core
+#sudo snap install core; sudo snap refresh core
 
 # Remove certbot-auto and any Certbot OS packages
 sudo apt-get remove certbot
 
 # Install Certbot
-sudo snap install --classic certbot
+# Run the following command, which will install two packages: certbot and python3-certbot-apache.
+# The latter is a plugin that integrates Certbot with Apache, so that it’s possible to automate obtaining a certificate and configuring HTTPS 
+# within your web server with a single command:
+sudo apt install certbot python3-certbot-apache
 
-# ensure that the certbot command can be run
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-
-# phát hành chứng chỉ cho cả domain yourdomain.com và toàn bộ các subdomain *.yourdomain.com
-sudo certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns -d yourdomain.com -d *.yourdomain.com
+# Obtaining an SSL Certificate
+# Certbot provides a variety of ways to obtain SSL certificates through plugins. 
+# The Apache plugin will take care of reconfiguring Apache and reloading the configuration whenever necessary:
+sudo certbot --apache
