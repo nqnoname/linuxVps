02517cab612bdf3f4bb0b8ec610646b14c19b4e4
nqnoname <115331491+nqnoname@users.noreply.github.com>
2022-11-12 09:19:49 +0700

    Update letEncryptWildcardSSL.sh

--- letEncryptWildcardSSL.sh
+++ letEncryptWildcardSSL.sh
@@ -9,3 +9,6 @@ sudo snap install --classic certbot
 
 # ensure that the certbot command can be run
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
+# phát hành chứng chỉ cho cả domain yourdomain.com và toàn bộ các subdomain *.yourdomain.com
+sudo certbot certonly --server https://acme-v02.api.letsencrypt.org/directory --manual --preferred-challenges dns -d yourdomain.com -d *.yourdomain.com
